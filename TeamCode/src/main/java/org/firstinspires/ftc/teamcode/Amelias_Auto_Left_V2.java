7ad26f2cc5db256bf9bb0edcca6e7379fbf7f603
pkaku99 <pkaku@hotmail.com>
2019-11-23 14:27:54 -0800

    Adding a comment for our code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Amelias_Auto_Left_V2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Amelias_Auto_Left_V2.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+//Amelia's Code v. 2
 
 @Autonomous (name="Autonomous Mode")
 
